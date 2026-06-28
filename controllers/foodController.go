ff6baedbc98b1b47c0ecbbff78e0f737c67b6893
Rishi <ramrijalrishi@gmail.com>
2026-06-28 19:39:46 +0300

    feat: added controllers for GetFoods, GetFood and CreateFood

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,19 +1,107 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/rishi-rijal/restuarant-management/database"
+	"github.com/rishi-rijal/restuarant-management/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var (
+	foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+	validate                         = validator.New()
+)
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error while listing the menu item"})
+			return
+		}
+		var allMenus []bson.M
+
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allMenus)
 	}
 }
 
 func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		foodID := c.Param("food_id")
+		var food models.Food
+
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodID}).Decode(&food)
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food"})
+			return
+		}
+
+		c.JSON(http.StatusOK, food)
 	}
 }
 
 func CreateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		defer cancel()
+		var menu models.Menu
+		var food models.Food
+
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(food)
+
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.MenuID}).Decode(&menu)
+		if err != nil {
+			msg := fmt.Sprintf("menu was not found")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		food.CreatedAt = time.Now()
+		food.UpdatedAt = time.Now()
+
+		food.ID = bson.NewObjectID()
+		food.FoodID = food.ID.Hex()
+		toFixed(*food.Price, 2)
+
+		result, insertErr := foodCollection.InsertOne(ctx, food)
+
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": insertErr.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
 	}
 }
 
